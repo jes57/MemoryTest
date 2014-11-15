1bd516045c3f77515e464bf1c50fdb61ab54b439
jes57 <jasonstephens1084@gmail.com>
2014-11-15 13:53:00 -0500

    ListView up and running.

--- app/src/main/java/com/garufa/memorytest/MainActivity.java
+++ app/src/main/java/com/garufa/memorytest/MainActivity.java
@@ -1,9 +1,16 @@
 package com.garufa.memorytest;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +19,29 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the array for the listView
+        Resources res = getResources();
+        String[] memoryTestArray = res.getStringArray(R.array.listView_array);
+
+        // Assign the array to the listView
+        final ListAdapter adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, memoryTestArray);
+
+        // Set the ListView
+        ListView memoryTestListView = (ListView) findViewById(R.id.quizListView);
+        memoryTestListView.setAdapter(adapter);
+
+        // Set the onClick event
+        memoryTestListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String testPicked = "You selected " +
+                        String.valueOf(parent.getItemAtPosition(position));
+
+                Toast.makeText(MainActivity.this, testPicked, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
